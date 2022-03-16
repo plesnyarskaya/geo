f7ef329ed176011e0fee16ba27a3a67e85bd6cf0
Plesnyarskaya Arina <plesnyarskayarina@yandex.ru>
2022-03-16 19:17:03 +0700

    First commit

---
+++ src/geometry/geometry.c
@@ -0,0 +1,15 @@
+#include "libgeo/circle.h"
+#include "libgeo/triangle.h"
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+int n;
+printf ("Figura: \n1.Krug\n2.Treugolnic\n");
+scanf ("%d",&n);
+if (n==1) circle();
+if (n==2) triangle();
+if ((n!=1)&&(n!=2)) printf ("ERROR\n");
+}
+
