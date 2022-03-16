cc65a632e055f4a82d65598c02249c9a01c82fc4
Plesnyarskaya Arina <plesnyarskayarina@yandex.ru>
2022-03-17 00:50:25 +0700

    321

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -8,16 +8,13 @@ int main()
     int n;
     printf("Figura: \n1.Krug\n2.Treugolnic\n");
     scanf("%d", &n);
-    if (n == 1)
-	{	
+    if (n == 1) {
         circle();
-	}
-    if (n == 2)
-	{
+    }
+    if (n == 2) {
         triangle();
-	}
-    if ((n != 1) && (n != 2))
-	{
+    }
+    if ((n != 1) && (n != 2)) {
         printf("ERROR\n");
-	}
+    }
 }
--- src/libgeo/triangle.c
+++ src/libgeo/triangle.c
@@ -8,14 +8,13 @@ void triangle()
     float s;
     printf("Vedite storoni:");
     scanf("%d %d %d", &a, &b, &c);
-    if ((a > 0) && (b > 0) && (c > 0))
-	{
+    if ((a > 0) && (b > 0) && (c > 0)) {
         perimetr = (a + b + c);
-        s = sqrt(perimetr / 2 * (perimetr / 2 - a) * (perimetr / 2 - b) * (perimetr / 2 - c));
+        s
+                = sqrt(perimetr / 2 * (perimetr / 2 - a) * (perimetr / 2 - b)
+                       * (perimetr / 2 - c));
         printf("\nperimetr = %d s = %.2f\n", perimetr, s);
-	} 
-   else 
-	{
+    } else {
         printf("NO\n");
-	}
+    }
 }
